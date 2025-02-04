991561ce10c8b452c9a096f714af81b1b0dd97a7
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-02-04 00:18:28 -0300

    done(shadcn ui and tailwind successfully installed)

--- produtos-sociais-ds-front/app/home/page.tsx
+++ produtos-sociais-ds-front/app/home/page.tsx
@@ -1,13 +1,9 @@
-// aqui vocês vão poder importar os componentes criados para testarem se o layout está bom!!!! mas no final nada disso vai ser usado, é só para visualização
+import { Button } from "@/components/ui/button"
 
-import React from 'react';
-
-const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
