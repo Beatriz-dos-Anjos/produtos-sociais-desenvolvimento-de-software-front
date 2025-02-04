c5c45ac9e963823596164178cf25cf7a477e6d7a
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-02-03 22:35:42 -0300

    feature(homePage added)

---
+++ produtos-sociais-ds-front/app/home/page.tsx
@@ -0,0 +1,13 @@
+// aqui vocês vão poder importar os componentes criados para testarem se o layout está bom!!!! mas no final nada disso vai ser usado, é só para visualização
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    );
+};
+
+export default Home;
