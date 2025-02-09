969821804f6345f3eb6b39aa01732d95bfed9a48
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-02-09 14:55:12 -0300

    feat(added components:button,dropdown and input field)

--- produtos-sociais-ds-front/app/dropdowns/page.tsx
+++ produtos-sociais-ds-front/app/dropdowns/page.tsx
@@ -1,10 +1,26 @@
-import Dropdown from "@/components/ui/dropdown";
+"use client"
+import { Dropdown } from "@/components/dropdown"
+export default function Home() {
+  const handleItemClick = (item: string) => {
+    console.log(`Selected: ${item}`)
+  }
 
-
-export default function DropdownUse () {
+  const dropdownItems = [
+    { label: "Unidade 1", onClick: () => handleItemClick("Unidade 1") },
+    { label: "Unidade 2", onClick: () => handleItemClick("Unidade 2") },
+    { label: "Unidade 3", onClick: () => handleItemClick("Unidade 3") },
+  ]
+  const dropdownItems2 = [
+    { label: "ONG 1", onClick: () => handleItemClick("ONG 1") },
+    { label: "ONG 2", onClick: () => handleItemClick("ONG 2") },
+    { label: "ONG 3", onClick: () => handleItemClick("ONG 3") },
+  ]
   return (
-    <div>
-      <Dropdown options={["Option 1", "Option 2", "Option 3"]} label="Select" />
+    <div className="min-h-screen flex items-center justify-center p-4 flex-col gap-4">
+      <Dropdown label="Unidades Disponíveis" placeholder="Placeholder" items={dropdownItems} />
+      <Dropdown label="ONGs Afiliadas" placeholder="Placeholder" items={dropdownItems2} />
+
     </div>
   )
 }
+
