7466b2e9e3fb420e5208bbe9a1126e4270d777ad
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-02-20 19:16:37 -0300

    feature: home-screen-added

--- produtos-sociais-ds-front/app/home/page.tsx
+++ produtos-sociais-ds-front/app/home/page.tsx
@@ -1,9 +1,27 @@
-import { Button } from "@/components/ui/button";
-
+import Header from "@/components/ui/headerNotLogged";
+import logo from "../../public/logo.svg";
+import Image from "next/image";
+import { DM_Sans } from "next/font/google";
+const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "700"] });
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex flex-col gap-4 ">
+      <Header />
+      <div className="flex flex-row gap-4">
+        <div className="flex flex-col gap-4 p-14 mt-12">
+          <Image src={logo} alt="Logo" />
+          <div
+            className="flex text-2xl font-thin items-center ml-24 mt-8"
+            style={{ fontFamily: dmSans.style.fontFamily }}
+          >
+            Artesanato único, feito com alma e coração
+            <br></br>
+            <br></br>
+            Descubra o encanto do feito à mão e apoie <br></br> talentos únicos:
+            explore agora nosso universo <br></br> de artesanatos com propósito.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
