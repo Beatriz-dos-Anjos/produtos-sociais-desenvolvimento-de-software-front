72b1db5de08151274f27fdac223633928a24ab03
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-03-13 20:00:15 -0300

    Created test for notification dropdown

--- produtos-sociais-ds-front/jest.config.ts
+++ produtos-sociais-ds-front/jest.config.ts
@@ -10,8 +10,14 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // 🔥 Corrigido para referenciar o arquivo de setup corretamente
+
+  transformIgnorePatterns: ["/node_modules/(?!lucide-react)"],
+
+  moduleNameMapper: {
+    "^lucide-react$":
+      "<rootDir>/node_modules/lucide-react/dist/cjs/lucide-react.js",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
